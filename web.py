610d24c73b11ac1c224bdd17dc47d6f73be9693c
blackexperiment <ar0497494@gmail.com>
2025-12-04 10:55:27 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,11 @@
+import os
 from flask import Flask
 
 web_app = Flask(__name__)
 
-@web_app.route('/')
+@web_app.route("/")
 def home():
-    return "Bot is running by SPIDY UNIVERSE"
+    return "Bot Running by SPIDY UNIVERSE"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-  
+    web_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
