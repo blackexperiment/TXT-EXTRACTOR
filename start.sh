ab8349fdad16c27ff8484022b692b753d80784a1
blackexperiment <ar0497494@gmail.com>
2025-12-04 10:57:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-echo "Cloning Repo..."
-git clone https://github.com/Popeye68/TXT-EXTRACTOR
-cd /TXT-EXTRACTOR
-pip install -r requirements.txt
-echo "Starting Bot..."
-python -m Extractor
+#!/bin/bash
+echo "Starting TXT-EXTRACTOR..."
+python main.py
